2e8d68621a09a912cdb67a285bdaf50419ec6613
chrisboenmgbhs <chrisboen8@outlook.com>
2026-03-12 12:16:08 +0800

    updated

---
+++ 20260218 WpfApp1/Model/LostItem.cs
@@ -0,0 +1,70 @@
+﻿using _20260218_WpfApp1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20260218_WpfApp1.Model
+{
+    internal class LostItem : ObservableObject
+    {
+        private string _name = string.Empty;
+        private string _description = string.Empty;
+        private string _location = string.Empty;
+        private string _time= string.Empty;
+
+        //REFERENCE TO PROPERTY CHANGE
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    OnPropertyChanged(nameof(Description));
+                }
+            }
+        }
+
+        public string Location
+        {
+            get => _location;
+            set
+            {
+                if (_location != value)
+                {
+                    _location = value;
+                    OnPropertyChanged(nameof(Location));
+                }
+            }
+        }
+
+        public string Time
+        {
+            get => _time;
+            set
+            {
+                if (_time != value)
+                {
+                    _time = value;
+                    OnPropertyChanged(nameof(Time));
+                }
+            }
+        }
+    }
+}
---
+++ 20260218 WpfApp1/ViewModel/NewWindow.cs
@@ -0,0 +1,61 @@
+﻿using _20260218_WpfApp1.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _20260218_WpfApp1.ViewModel
+{
+    internal class NewWindow
+    {
+        public LostItem LostItem{ get; set; }
+        public ICommand SaveCommand { get; set; }
+
+        public ObservableCollection<LostItem> LostItems { get; set; }
+
+        public NewWindow()
+        {
+            LostItem = new LostItem();
+            SaveCommand = new RelayCommand(SaveLostItem);
+
+            LostItems = new ObservableCollection<LostItem>()
+            { 
+                new LostItem
+                {   
+                    Name = "Wallet",
+                    Description = "Black leather wallet with multiple card slots and a zippered coin pocket.",
+                    Location = "Central Park",
+                    Time = "2024-06-15 14:30"
+                },
+
+                new LostItem
+                {
+                    Name = "iPhone",
+                    Description = "Apple phone",
+                    Location = "Library",
+                    Time = "2024-06-16 10:00"
+                }
+            };
+        }
+
+        private void SaveLostItem()
+        {
+            LostItems.Add(new LostItem
+            {
+                Name = LostItem.Name,
+                Description = LostItem.Description,
+                Location = LostItem.Location,
+                Time = LostItem.Time
+            });
+
+            // Clear the input fields after saving
+            LostItem.Name = string.Empty;
+            LostItem.Description = string.Empty;
+            LostItem.Location = string.Empty;
+            LostItem.Time = string.Empty;
+        }
+    }
+}
