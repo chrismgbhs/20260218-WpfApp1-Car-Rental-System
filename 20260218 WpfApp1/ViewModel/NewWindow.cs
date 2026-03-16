648bca3fde85883d557c30777c3edc51c66d2461
chrisboenmgbhs <chrisboen8@outlook.com>
2026-03-16 12:53:01 +0800

    updated

--- 20260218 WpfApp1/ViewModel/NewWindow.cs
+++ 20260218 WpfApp1/ViewModel/NewWindow.cs
@@ -5,21 +5,46 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Input;
 
 namespace _20260218_WpfApp1.ViewModel
 {
-    internal class NewWindow
+    internal class NewWindow : ObservableObject
     {
         public LostItem LostItem{ get; set; }
         public ICommand SaveCommand { get; set; }
+        public ICommand ClearCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
 
         public ObservableCollection<LostItem> LostItems { get; set; }
+
+        private LostItem _selectedLostItems;
+        public LostItem SelectedLostItems
+        {
+            get { return _selectedLostItems; }
+            set
+            {
+                _selectedLostItems = value;
+                OnPropertyChanged(nameof(SelectedLostItems));
+
+                if (SelectedLostItems != null)
+                {
+                    LostItem.Name = SelectedLostItems.Name;
+                    LostItem.Description = SelectedLostItems.Description;
+                    LostItem.Location = SelectedLostItems.Location;
+                    LostItem.Time = SelectedLostItems.Time;
+                }
+            }
+        }
 
         public NewWindow()
         {
             LostItem = new LostItem();
             SaveCommand = new RelayCommand(SaveLostItem);
+            ClearCommand = new RelayCommand(ClearFields);
+            DeleteCommand = new RelayCommand(DeleteLostItem);
 
             LostItems = new ObservableCollection<LostItem>()
             { 
@@ -43,19 +68,47 @@ namespace _20260218_WpfApp1.ViewModel
 
         private void SaveLostItem()
         {
-            LostItems.Add(new LostItem
+            if (LostItem.Name == "" || LostItem.Description == "" || LostItem.Location == "")
             {
-                Name = LostItem.Name,
-                Description = LostItem.Description,
-                Location = LostItem.Location,
-                Time = LostItem.Time
-            });
+                MessageBox.Show("Please fill in all fields before saving.");
+            }
 
-            // Clear the input fields after saving
+            else
+            {
+                LostItems.Add(new LostItem
+                {
+                    Name = LostItem.Name,
+                    Description = LostItem.Description,
+                    Location = LostItem.Location,
+                    Time = DateTime.Now.ToString("yyyy-MM-dd HH:mm")
+                });
+
+                // Clear the input fields after saving
+                ClearFields();
+            }
+        }
+
+        private void ClearFields()
+        {
             LostItem.Name = string.Empty;
             LostItem.Description = string.Empty;
             LostItem.Location = string.Empty;
             LostItem.Time = string.Empty;
+        }
+
+        private void DeleteLostItem()
+        {
+
+           if (SelectedLostItems != null)
+            {
+                LostItems.Remove(SelectedLostItems);
+                ClearFields();
+            }
+
+            else
+            {
+                MessageBox.Show("Please select an item to delete.");
+            }
         }
     }
 }
