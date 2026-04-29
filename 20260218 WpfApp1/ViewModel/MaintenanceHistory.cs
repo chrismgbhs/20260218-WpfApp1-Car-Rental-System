e1ad1893ccd521d71dad59e690c73e29f7a098ad
chrisboenmgbhs <chrisboen8@outlook.com>
2026-04-29 08:30:24 +0800

    ff

--- 20260218 WpfApp1/ViewModel/MaintenanceHistory.cs
+++ 20260218 WpfApp1/ViewModel/MaintenanceHistory.cs
@@ -2,6 +2,7 @@
 using _20260218_WpfApp1.View;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,9 @@ namespace _20260218_WpfApp1.ViewModel
 {
     internal class MaintenanceHistory : ObservableObject
     {
-        public string _history;
         public string PlateNumber { get; set; }
         public ICommand SearchCommand { get; set; }
         public ICommand BackCommand { get; set; }
-
-        public string History
-        {
-            get { return _history; }
-            set { _history = value; OnPropertyChanged(nameof(History)); }
-        }
 
         public MaintenanceHistory()
         {
@@ -33,10 +27,8 @@ namespace _20260218_WpfApp1.ViewModel
         {
             File_Manager file_Manager = new File_Manager($"File/{PlateNumber}.csv");
             List<string> lines = file_Manager.getLines();
-            foreach (string line in lines)
-            {
-                History += $"{line}\n";
-            }
+            file_Manager.Write(lines, true);
+            MessageBox.Show($"Receipt has been printed on Debug/File/{PlateNumber}.csv");
         }
 
         public void ExecuteBack()
